079365e3c62a9d3b006d039e0c1fe0757d8fc6e9
WinstonTan <winston.tc.han@gmail.com>
2021-05-10 20:09:35 +0800

    Adding Google Sheet test class and functions

---
+++ src/test/java/util/GoogleSheetHelper.java
@@ -0,0 +1,76 @@
+package util;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.ValueRange;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.List;
+
+public class GoogleSheetHelper {
+    private static Sheets sheetService;
+    private static String APPLICATION_NAME = "Test Data Sheet";
+    private static String SPREADSHEET_ID = "1lFxOjbr9lTaMUwc3_0f_zu2_c-FT5SXDKsVg-tqWCWk";
+
+    private static Credential authorize() throws IOException, GeneralSecurityException {
+        InputStream in = GoogleSheetHelper.class.getResourceAsStream("/credentials.json");
+
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
+                JacksonFactory.getDefaultInstance(), new InputStreamReader(in));
+
+        List<String> scopes = Arrays.asList(SheetsScopes.SPREADSHEETS);
+
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(), JacksonFactory.getDefaultInstance(),
+                clientSecrets, scopes)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File("tokens")))
+                .setAccessType("offline")
+                .build();
+
+        Credential credential = new AuthorizationCodeInstalledApp(
+                flow, new LocalServerReceiver())
+                .authorize("user");
+
+        return credential;
+    }
+
+    public static Sheets getSheetService() throws IOException, GeneralSecurityException {
+        Credential credential = authorize();
+        return new Sheets.Builder(GoogleNetHttpTransport.newTrustedTransport(),
+                JacksonFactory.getDefaultInstance(), credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+    public static void main(String[] args) throws IOException, GeneralSecurityException {
+        sheetService = getSheetService();
+        String range = "Sheet1!A2:F10";
+
+        ValueRange response = sheetService.spreadsheets().values()
+                .get(SPREADSHEET_ID, range)
+                .execute();
+
+        List<List<Object>> values = response.getValues();
+
+        if(values == null || values.isEmpty()) {
+            System.out.println("No data found.");
+        } else {
+            for (List row : values) {
+                System.out.printf("%s %s", row.get(0), row.get(1));
+            }
+        }
+    }
+}
