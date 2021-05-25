ec28632aeb2021066a60924e72ac0780b6cfd382
WinstonTan <winston.tc.han@gmail.com>
2021-05-25 10:45:47 +0800

    Changed credentials for Google Sheet API

--- src/test/java/util/GoogleSheetHelper.java
+++ src/test/java/util/GoogleSheetHelper.java
@@ -12,7 +12,6 @@ import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.ValueRange;
 
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
